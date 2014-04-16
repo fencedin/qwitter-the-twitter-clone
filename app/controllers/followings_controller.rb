a5c381b752f74f0a4708f057d821d0f26a25a32f
vincent rotter & Michael Grossman <student@epicodus.com>
2014-04-15 17:41:21 -0700

    add ability to unfollow users

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -6,6 +6,14 @@ class FollowingsController < ApplicationController
     redirect_to :back
   end
 
+  def destroy
+    @user = User.find(params[:following][:user_id])
+    @following = User.find(params[:following][:follower_id])
+    @followinger = Following.where(user_id: @user.id, follower_id: @following.id).first
+    @followinger.destroy
+    redirect_to :back
+  end
+
   private
   def following_params
     params.require(:following).permit(:user_id, :follower_id)
